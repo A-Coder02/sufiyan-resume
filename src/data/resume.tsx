43ee1486a576b1fb308c6009ce78bad0e9c63a2f
Arbaj-Dev <65939474+A-Coder02@users.noreply.github.com>
2026-04-04 00:59:54 +0530

    hotfix 6

--- src/data/resume.tsx
+++ src/data/resume.tsx
@@ -122,7 +122,7 @@ export const DATA = {
   education: [
     {
       school: "Aki’s Poona College of Arts, Science & Commerce",
-      href: "#",
+      href: "https://i1.wp.com/poonacollege.edu.in/wp-content/uploads/2021/03/cropped-cropped-logo-50-gulden-jubilee-year.png?fit=512%2C512&ssl=1",
       degree: "Bachelor of Computer Application (CGPA: 8.77)",
       logoUrl: "/college.png",
       start: "2020",
